1919114e65bdb1b7f25dd33a14594379817baaf3
Rodrigo Cabral <37848619+rodcabral@users.noreply.github.com>
2025-10-15 21:17:44 -0300

    create container in layout

--- app/components/layout.tsx
+++ app/components/layout.tsx
@@ -1,12 +1,14 @@
 import Header from "./Header"
 import Footer from "./Footer"
 
-export default function Layout({ children }: any) {
+export default function Layout({ children }: { children: React.ReactNode }) {
     return (
-        <div>
-            <Header />
-            <main>{children}</main>
-            <Footer />
+        <div className="w-full flex flex-col items-center selection:bg-green-700 font-sans">
+            <div className="max-w-4xl flex flex-col gap-12 p-8 lg:p-20 lg:py-28">
+                <Header />
+                <main>{children}</main>
+                <Footer />
+            </div>
         </div>
     )
 }
