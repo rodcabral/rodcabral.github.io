ffafe7752fac08988f2235e8ec85be38cde7118c
Rodrigo Cabral <37848619+rodcabral@users.noreply.github.com>
2025-10-16 04:30:47 -0300

    conditional styling based on pathname

--- app/components/Header/index.tsx
+++ app/components/Header/index.tsx
@@ -1,9 +1,13 @@
+"use client"
+
 import Link from "next/link"
+import { usePathname } from "next/navigation"
 
 function MenuItem({ title, href }: { title: string, href: string }) {
+    const pathname = usePathname();
     return (
         <li className="rounded-full px-4 py-1">
-            <Link href={href} className="rounded-full px-4 py-1 hover:bg-white hover:text-black">{title}</Link>
+            <Link href={href} className={`rounded-full px-4 py-1 hover:bg-white hover:text-black ${pathname == href ? 'bg-white  text-black' : ''}`}>{title}</Link>
         </li>
     )
 }
--- app/components/layout.tsx
+++ app/components/layout.tsx
@@ -4,7 +4,7 @@ import Footer from "./Footer"
 export default function Layout({ children }: { children: React.ReactNode }) {
     return (
         <div className="w-full flex flex-col items-center selection:bg-green-700 font-sans">
-            <div className="max-w-4xl flex flex-col gap-12 p-8 lg:p-20 lg:py-28">
+            <div className="md:w-4xl lg:w-4xl sm:max-w-4xl flex flex-col gap-12 p-8 lg:p-20 lg:py-28">
                 <Header />
                 <main>{children}</main>
                 <Footer />
