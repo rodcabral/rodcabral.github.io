a73db7221ec3cc7b511112927547ae51b514ad82
Rodrigo Cabral <37848619+rodcabral@users.noreply.github.com>
2025-10-15 21:01:24 -0300

    layout

---
+++ app/components/Footer/index.tsx
@@ -0,0 +1,7 @@
+export default function Footer() {
+    return (
+        <footer className="w-full py-10 max-w-4xl p-8 lg:px-15">
+            <p className="font-mono text-xs text-gray-300">Copyright © 2025 Rodrigo Cabral. All rights reserved.</p>
+        </footer>
+    )
+}
