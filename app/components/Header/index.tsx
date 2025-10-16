87179d5f3440071ac00283342f9d0bb25ad88fd0
Rodrigo Cabral <37848619+rodcabral@users.noreply.github.com>
2025-10-15 22:54:38 -0300

    create header and about me

--- app/components/Header/index.tsx
+++ app/components/Header/index.tsx
@@ -1,7 +1,30 @@
+import Link from "next/link"
+
+function MenuItem({ title, href }: { title: string, href: string }) {
+    return (
+        <li className="rounded-full px-4 py-1">
+            <Link href={href} className="rounded-full px-4 py-1 hover:bg-white hover:text-black">{title}</Link>
+        </li>
+    )
+}
+
+function Menu() {
+    return (
+        <ul className="flex gap-2 items-center align-middle font-mono">
+            <MenuItem title="Home" href="/" />
+            <MenuItem title="Blog" href="/blog" />
+            <MenuItem title="Projects" href="/projects" />
+        </ul>
+    )
+}
+
 export default function Header() {
     return (
-        <div>
-            Header
+        <div className="flex justify-between items-center align-middle">
+            <div>
+                <p className="font-bold">Rodrigo Cabral</p>
+            </div>
+            <Menu />
         </div>
     )
 }
--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,23 @@
 import Layout from "./components/layout"
 
+function Focus({ txt }: { txt: string }) {
+    return (
+        <span className="text-green-700 selection:text-white">{txt}</span>
+    )
+}
+
 export default function Home() {
     return (
         <Layout>
-            <div>
-                <p>Hello, world!</p>
+            <div className="flex flex-col gap-5">
+                <div className="flex flex-col gap-2">
+                    <h4 className="font-semibold">Hi, I'm Rodrigo</h4>
+                    <p className="text-gray-300">I'm a Software Developer from Brazil, dedicated to creating high-performance code.</p>
+                </div>
+                <p className="text-gray-300 text-base/loose">I am enthusiastic about building projects that solve real-world problems. Mostly working with <Focus txt="Typescript" />, <Focus txt="C" />, <Focus txt="C++" /> and <Focus txt="Python" /> to build modern software like Developer Tools or Full-Stack applications, but I always enjoy learning new technologies and languages.</p>
+
+                <p className="text-gray-300">I'm also extremely interested in Low-Level, Computer Graphics and Machine Learning.</p>
             </div>
-        </Layout>
+        </Layout >
     )
 }
