9bea0985bb0d7291c45582f6bd6b435297e8d7d2
Rodrigo Cabral <37848619+rodcabral@users.noreply.github.com>
2025-10-15 22:54:54 -0300

    fix footer

--- app/components/Footer/index.tsx
+++ app/components/Footer/index.tsx
@@ -1,6 +1,6 @@
 export default function Footer() {
     return (
-        <footer className="w-full py-10 max-w-4xl p-8 lg:px-15">
+        <footer className="w-full">
             <p className="font-mono text-xs text-gray-300">Copyright © 2025 Rodrigo Cabral. All rights reserved.</p>
         </footer>
     )
