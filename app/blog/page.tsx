94ea9cc9c5909858ed8a7faf2f2af000770ac5d0
Rodrigo Cabral <37848619+rodcabral@users.noreply.github.com>
2025-10-17 15:15:59 -0300

    temporary blog

--- app/blog/page.tsx
+++ app/blog/page.tsx
@@ -4,10 +4,10 @@ import BlogTitle from "../components/Blog/title"
 export default function Blog() {
     return (
         <Layout>
-            <h4 className="font-semibold">Blog posts</h4>
-            <div className="mt-5 flex flex-col gap-5">
-                <BlogTitle date="October 16, 25" title="Lorem ipsum dolor sit amet" />
-                <BlogTitle date="October 16, 25" title="Lorem ipsum dolor sit amet" />
+            <div className="flex flex-col gap-5">
+                <BlogTitle date="Oct 16, 25" title="Lorem ipsum dolor sit amet" />
+                <BlogTitle date="Oct 16, 25" title="Lorem ipsum dolor" />
+                <BlogTitle date="Oct 16, 25" title="Hello, World!" />
             </div>
         </Layout>
     )
