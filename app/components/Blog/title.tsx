b052e2a0723ceb3cf0f942993cbb7aae38345dd0
Rodrigo Cabral <37848619+rodcabral@users.noreply.github.com>
2025-10-17 12:35:52 -0300

    fix header and blog title

--- app/components/Blog/title.tsx
+++ app/components/Blog/title.tsx
@@ -1,8 +1,8 @@
 export default function BlogTitle({ date, title }: { date: string, title: string }) {
     return (
-        <div className="flex gap-3 items-center duration-200 hover:bg-green-900 cursor-pointer">
-            <span className="bg-green-600 w-1 h-12 rounded-full"></span>
-            <div className="flex flex-col">
+        <div className="flex gap-3 items-center duration-200 hover:bg-green-950 cursor-pointer">
+            <span className="bg-green-800 w-1 h-10 rounded-full"></span>
+            <div className="flex flex-col py-1">
                 <p className="text-xs text-gray-600">{date}</p>
                 <p className="font-mono text-gray-300">{title}</p>
             </div>
